ccb91833f2dc51c4978cf072dba9681d66fdbc06
Stiven Muñoz Murillo <stivenmm1993@gmail.com>
2019-04-26 13:35:11 -0500

    ¿proyecto terminado?

--- js/archivo.js
+++ js/archivo.js
@@ -80,48 +80,87 @@ function consultar() {
             // Muestro el mensaje de error de la BD
             console.log(respuestaJSON["MENSAJE"]);
         } else {
-            console.log(respuestaJSON["MENSAJE"]);
+            var mensaje = respuestaJSON["MENSAJE"];
+            var filas = mensaje.split("-");
+            var ANCHO = 0;
 
-            /*
-            var re = /\(\d+,\d+\)/g;
-            var str = respuestaJSON["RECORRIDO"];
-            var recorrido = str.match(re);
-            console.log(recorrido.length);
-            console.log(recorrido);
+            // Elimino el último elemento
+            filas.pop();
 
-            var contador = 0;
-            $("#camino").html("CAMINO: &nbsp;");
-            */
+            // Obtengo el elemento identificado como tabla
+            var tabla = document.getElementById("tabla");
 
-            /*
-            var intervalId = setInterval(function () {
-                // Detengo el procesamiento del recorrido
-                if (contador < recorrido.length) {
-                    if (id_actual == `c(${X2},${Y2})`) {
-                        camino_finalizado = true;
-                        document.getElementById("audio_salida").play();
-                        document.getElementById(id_actual).src = "includes/img/rpg_maker/abierta.png";
+            // Borro su contenido
+            tabla.innerHTML = "";
+
+            // Hago un primer ciclo para crear y recorrer 
+            // las filas de la tabla según el ALTO
+            console.log(filas);
+            for (let y = 0; y < filas.length; y++) {
+                // Creo un elemento tr (table row)
+                var tr = document.createElement("tr");
+
+                // En la primera columna de cada fila
+                var td = document.createElement("td");
+                // Almaceno la guardo posición de dicha fila
+                td.innerHTML = `<p>${y} &nbsp;</p>`;
+                // Y agrego es primera columna a la tabla
+                tr.appendChild(td);
+
+                // Hago un segundo ciclo para crear y recorrer las columnas 
+                // de la tabla según el ANCHO en cada fila
+                var columnas = filas[y].split(";");
+                ANCHO = columnas.length;
+                for (let x = 0; x < columnas.length; x++) {
+                    // Creo un elemento td (colummna tabla - fila)
+                    var td = document.createElement("td");
+
+                    var img;
+                    if (columnas[x] == "1") {
+                        // Los bordes de la tabla con espacios
+                        img = `<img id='c(${x},${y})' src="../includes/img/rpg_maker/espacio.png">`;
+                    } else if (columnas[x] == "0") {
+                        // El resto de la tabla son paredes
+                        img = `<img id='c(${x},${y})' src="../includes/img/rpg_maker/pared.png">`;
                     } else {
-                        if (camino_finalizado) {
-                            document.getElementById(id_actual).src = "includes/img/rpg_maker/espacio.png";
-                        } else {
-                            document.getElementById(id_actual).src = "includes/img/rpg_maker/camino.png";
-                            $("#camino").text($("#camino").text() + recorrido[contador] + " ");
-                        }
+                        img = `<img id='c(${x},${y})' src="../includes/img/rpg_maker/cerrada.png">`;
                     }
 
-                    id_actual = "c" + recorrido[contador];
-                    document.getElementById(id_actual).src = "includes/img/rpg_maker/toro.png";
+                    // Cada columna de cada fila guarda un botón que informa
+                    // su posición a la función coordenada y almacena una imagen
+                    td.innerHTML = `<button onclick='coordenada(${x},${y})'>${img}</button>`;
 
-                    contador++;
-                } else {
-                    clearInterval(intervalId);
-                    camino_finalizado = false;
-                    var cadena_recorrido = "&nbsp;-&nbsp; RECORRIDO: &nbsp;" + recorrido.join(" ");
-                    $("#camino").html($("#camino").html() + cadena_recorrido);
+                    // Agrego las columnas a la fila actual
+                    tr.appendChild(td);
                 }
-            }, 1000);
-            */
+
+                // Almaceno la fila actual con sus respectivas columnas
+                // y procedo a seguir cargando la información del resto
+                // de filas, hasta que el ciclo termine
+                tabla.appendChild(tr);
+            }
+
+            // Creo una nueva fila
+            var tr = document.createElement("tr");
+            // Creo un nuevo elemento columna de fila
+            var td = document.createElement("td");
+            // Almaceno un espacio en blanco al 
+            // inicio de esta última fila
+            td.innerHTML = `<p> </p>`;
+            // Agrego dicho espacio en blanco a la fila
+            tr.appendChild(td);
+
+            // Sigo agregando información a la misma fila
+            for (let x = 0; x < ANCHO; x++) {
+                // Agrego la posición en el eje x
+                // a esta última fila
+                var td = document.createElement("td");
+                td.innerHTML = `<p>${x}</p>`;
+                tr.appendChild(td);
+            }
+
+            // Agrego la última fila de la tabla.
+            tabla.appendChild(tr);
         }
     });
 
@@ -139,76 +178,4 @@ function coordenada(x, y) {
 
     // Reproducir audio precargado en  el index.html
     document.getElementById("audio_seleccionar").play();
-}
-
-function cargar_laberinto() {
-    // Obtengo el elemento identificado como tabla
-    var tabla = document.getElementById("tabla");
-
-    // Borro su contenido
-    tabla.innerHTML = "";
-
-    // Hago un primer ciclo para crear y recorrer 
-    // las filas de la tabla según el ALTO
-    for (let y = 0; y < ALTO; y++) {
-        // Creo un elemento tr (table row)
-        var tr = document.createElement("tr");
-
-        // En la primera columna de cada fila
-        var td = document.createElement("td");
-        // Almaceno la guardo posición de dicha fila
-        td.innerHTML = `<p>${y} &nbsp;</p>`;
-        // Y agrego es primera columna a la tabla
-        tr.appendChild(td);
-
-        // Hago un segundo ciclo para crear y recorrer las columnas 
-        // de la tabla según el ANCHO en cada fila
-        for (let x = 0; x < ANCHO; x++) {
-            // Creo un elemento td (colummna tabla - fila)
-            var td = document.createElement("td");
-
-            var img;
-            if (x == 0 || x == ANCHO - 1 || y == 0 || y == ALTO - 1) {
-                // Los bordes de la tabla con espacios
-                img = `<img id='c(${x},${y})' src="includes/img/rpg_maker/espacio.png">`;
-            } else {
-                // El resto de la tabla son paredes
-                img = `<img id='c(${x},${y})' src="includes/img/rpg_maker/pared.png">`;
-            }
-
-            // Cada columna de cada fila guarda un botón que informa
-            // su posición a la función coordenada y almacena una imagen
-            td.innerHTML = `<button onclick='coordenada(${x},${y})'>${img}</button>`;
-
-            // Agrego las columnas a la fila actual
-            tr.appendChild(td);
-        }
-
-        // Almaceno la fila actual con sus respectivas columnas
-        // y procedo a seguir cargando la información del resto
-        // de filas, hasta que el ciclo termine
-        tabla.appendChild(tr);
-    }
-
-    // Creo una nueva fila
-    var tr = document.createElement("tr");
-    // Creo un nuevo elemento columna de fila
-    var td = document.createElement("td");
-    // Almaceno un espacio en blanco al 
-    // inicio de esta última fila
-    td.innerHTML = `<p> </p>`;
-    // Agrego dicho espacio en blanco a la fila
-    tr.appendChild(td);
-
-    // Sigo agregando información a la misma fila
-    for (let x = 0; x < ANCHO; x++) {
-        // Agrego la posición en el eje x
-        // a esta última fila
-        var td = document.createElement("td");
-        td.innerHTML = `<p>${x}</p>`;
-        tr.appendChild(td);
-    }
-
-    // Agrego la última fila de la tabla.
-    tabla.appendChild(tr);
 }
